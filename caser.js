6837b2d0b422c2f6b9664feedb8092426f2054d5
Anton Khorev <tony29@yandex.ru>
2020-04-25 01:08:08 +0300

    put all requests in queue; can check multiple elements per case

--- caser.js
+++ caser.js
@@ -5,17 +5,6 @@ const expat=require('node-expat')
 const osm=require('./osm')
 const User=require('./user')
 
-const filename=process.argv[2]
-
-if (filename===undefined) {
-	console.log('missing cases filename')
-	return process.exit(1)
-}
-
-let match
-let inCaseSectionLevel=0
-let readingCaseData
-
 function checkElementTags(elementType,elementId,tags,callback) {
 	const diff={}
 	for (const [k,v] of Object.entries(tags)) {
@@ -36,90 +25,128 @@ function checkElementTags(elementType,elementId,tags,callback) {
 	})
 }
 
-function closeCase(caseData) {
-	const writeCaseTitle=()=>console.log(`## case #${caseData.id} ${caseData.name}`)
+function processCase(caseData,callback) {
+	console.log(`## case #${caseData.id} ${caseData.name}`)
 	if (caseData.uid) {
 		if (!caseData.changesetsCount) {
-			writeCaseTitle()
 			console.log(`* changesets count not set`)
+			callback()
 			return
 		}
 		const user=new User(caseData.uid)
 		user.requestMetadata(()=>{
-			writeCaseTitle()
 			if (user.changesetsCount>Number(caseData.changesetsCount)) {
 				console.log(`* USER MADE EDITS`)
 			} else {
 				console.log(`* user made no edits`)
 			}
+			callback()
 		})
-	} else if (caseData.elementType && caseData.elementId) {
+	} else if (caseData.elements) {
 		if (!caseData.tags || Object.keys(caseData.tags).length===0) {
-			writeCaseTitle()
 			console.log(`* no tags to check`)
+			callback()
 			return
 		}
-		checkElementTags(caseData.elementType,caseData.elementId,caseData.tags,diff=>{
-			writeCaseTitle()
-			if (Object.keys(diff).length===0) {
-				console.log(`* no tag differences`)
+		const rec=(i)=>{
+			if (i>=caseData.elements.length) {
+				callback()
 				return
 			}
-			for (const [k,[v1,v2]] of Object.entries(diff)) {
-				console.log(`* EXPECTED TAG ${k}=${v1}`)
-				console.log(`* ACTUAL   TAG ${k}=${v2}`)
-			}
-		})
+			const [elementType,elementId]=caseData.elements[i]
+			console.log(`### ${elementType} #${elementId}`)
+			checkElementTags(elementType,elementId,caseData.tags,diff=>{
+				if (Object.keys(diff).length===0) {
+					console.log(`* no tag differences`)
+				} else {
+					for (const [k,[v1,v2]] of Object.entries(diff)) {
+						console.log(`* EXPECTED TAG ${k}=${v1}`)
+						console.log(`* ACTUAL   TAG ${k}=${v2}`)
+					}
+				}
+				rec(i+1)
+			})
+		}
+		rec(0)
 	} else {
-		writeCaseTitle()
 		console.log(`* uid/element not set`)
+		callback()
 	}
 }
 
-function parseElementString(elementString) {
+function processCases(caseDataQueue,callback) {
+	const rec=(i)=>{
+		if (i>=caseDataQueue.length) {
+			callback()
+			return
+		}
+		processCase(caseDataQueue[i],()=>{
+			rec(i+1)
+		})
+	}
+	rec(0)
+}
+
+function readCases(filename,callback) {
+	function parseElementString(elementString) {
+		let match
+		if (match=elementString.match(/(node|way|relation)\/(\d+)$/)) {
+			const [,type,id]=match
+			return [type,id]
+		} else {
+			return [undefined,undefined]
+		}
+	}
 	let match
-	if (match=elementString.match(/(node|way|relation)\/(\d+)$/)) {
-		const [,type,id]=match
-		return [type,id]
-	} else {
-		return [undefined,undefined]
-	}
+	let inCaseSectionLevel=0
+	let caseDataQueue=[]
+	let readingCaseData
+	readline.createInterface({
+		input: fs.createReadStream(filename)
+	}).on('line',input=>{
+		if (match=input.match(/^(#+)(.*)/)) {
+			const [,headerPrefix,rest]=match
+			const sectionLevel=headerPrefix.length
+			if (sectionLevel<=inCaseSectionLevel) {
+				caseDataQueue.push(readingCaseData)
+				readingCaseData=undefined
+				inCaseSectionLevel=0
+			}
+			if (inCaseSectionLevel<=0 && (match=rest.match(/\s*#(\S+)\s+(.*)/))) {
+				inCaseSectionLevel=sectionLevel
+				readingCaseData={}
+				;[,readingCaseData.id,readingCaseData.name]=match
+			}
+		}
+		if (inCaseSectionLevel<=0) return
+		if (match=input.match(/^\*\s+uid\s+(\S+)/)) {
+			;[,readingCaseData.uid]=match
+		} else if (match=input.match(/^\*\s+changesets\s+count\s+(\S+)/)) {
+			;[,readingCaseData.changesetsCount]=match
+		} else if (match=input.match(/^\*\s+element\s+(\S+)/)) {
+			const [,elementString]=match
+			if (!readingCaseData.elements) readingCaseData.elements=[]
+			readingCaseData.elements.push(parseElementString(elementString))
+		} else if (match=input.match(/^\*\s+tag\s+([^=]+)=(.*)$/)) {
+			const [,k,v]=match
+			if (!readingCaseData.tags) readingCaseData.tags={}
+			readingCaseData.tags[k]=v
+		}
+	}).on('close',()=>{
+		if (inCaseSectionLevel>0) {
+			caseDataQueue.push(readingCaseData)
+			readingCaseData=undefined
+			inCaseSectionLevel=0
+		}
+		callback(caseDataQueue)
+	})
 }
 
-readline.createInterface({
-	input: fs.createReadStream(filename)
-}).on('line',input=>{
-	if (match=input.match(/^(#+)(.*)/)) {
-		const [,headerPrefix,rest]=match
-		const sectionLevel=headerPrefix.length
-		if (sectionLevel<=inCaseSectionLevel) {
-			closeCase(readingCaseData)
-			inCaseSectionLevel=0
-			readingCaseData=undefined
-		}
-		if (inCaseSectionLevel<=0 && (match=rest.match(/\s*#(\S+)\s+(.*)/))) {
-			inCaseSectionLevel=sectionLevel
-			readingCaseData={}
-			;[,readingCaseData.id,readingCaseData.name]=match
-		}
-	}
-	if (inCaseSectionLevel<=0) return
-	if (match=input.match(/^\*\s+uid\s+(\S+)/)) {
-		;[,readingCaseData.uid]=match
-	} else if (match=input.match(/^\*\s+changesets\s+count\s+(\S+)/)) {
-		;[,readingCaseData.changesetsCount]=match
-	} else if (match=input.match(/^\*\s+element\s+(\S+)/)) {
-		const [,elementString]=match
-		;[readingCaseData.elementType,readingCaseData.elementId]=parseElementString(elementString)
-	} else if (match=input.match(/^\*\s+tag\s+([^=]+)=(.*)$/)) {
-		const [,k,v]=match
-		if (!readingCaseData.tags) readingCaseData.tags={}
-		readingCaseData.tags[k]=v
-	}
-}).on('close',()=>{
-	if (inCaseSectionLevel>0) {
-		closeCase(readingCaseData)
-		inCaseSectionLevel=0
-		readingCaseData=undefined
-	}
+const filename=process.argv[2]
+if (filename===undefined) {
+	console.log('missing cases filename')
+	return process.exit(1)
+}
+readCases(filename,(caseDataQueue)=>{
+	processCases(caseDataQueue,()=>{})
 })
