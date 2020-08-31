f879607b3ead6ea3141b69f5eb9f779864fc8214
Anton Khorev <tony29@yandex.ru>
2020-08-31 18:01:27 +0300

    write dummy file

--- relation2.js
+++ relation2.js
@@ -1,18 +1,24 @@
 // explore relations in changeset
-// node relation2.js <changeset url> [<store file>]
+// node relation2.js <changeset url> <output html file> [<store file>]
 
-if (process.argv[2]===undefined) {
+const fs=require('fs')
+const e=require('./escape')
+const osm=require('./osm')
+
+if (process.argv[2]===undefined || process.argv[3]===undefined) {
 	console.log('invalid args')
 	return process.exit(1)
 }
-main(process.argv[2],process.argv[3])
+main(process.argv[2],process.argv[3],process.argv[4])
 
-async function main(changesetUrl,storeFilename) {
+async function main(changesetUrl,outputFilename,storeFilename) {
 	const changesetId=parseChangesetUrl(changesetUrl)
 	if (storeFilename===undefined) {
 		storeFilename=`relations-in-cset-${changesetId}.json`
 	}
-	console.log('cset',changesetId,'fname',storeFilename)
+	console.log(`exploring changeset ${changesetId}, using store file ${storeFilename}`)
+	const store=readStore(storeFilename)
+	await writeReport(changesetId,store,outputFilename)
 }
 
 function parseChangesetUrl(changesetUrl) {
@@ -29,5 +35,32 @@ function parseChangesetUrl(changesetUrl) {
 	} else {
 		console.log(`unrecognized host ${url.host}`)
 		return process.exit(1)
+	}
+}
+
+function readStore(storeFilename) {
+	if (fs.existsSync(storeFilename)) {
+		return JSON.parse(fs.readFileSync(storeFilename))
+	} else {
+		return osm.makeStore()
+	}
+}
+
+async function writeReport(changesetId,store,outputFilename) {
+	const reportFile=fs.createWriteStream(outputFilename)
+	const title=`Changes in relations caused by changeset ${changesetId}`
+	for (const line of [
+		`<!DOCTYPE html>`,
+		`<html lang=en>`,
+		`<head>`,
+		`<meta charset=utf-8>`,
+		e.h`<title>${title}</title>`,
+		`</head>`,
+		`<body>`,
+		`</body>`,
+		`</html>`,
+	]) {
+		reportFile.write(line)
+		reportFile.write('\n')
 	}
 }
