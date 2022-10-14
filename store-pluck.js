bd03fa81a4af2550b281d876b94c3a6341d08a08
Anton Khorev <tony29@yandex.ru>
2022-10-14 04:31:32 +0300

    pluck id range

--- store-pluck.js
+++ store-pluck.js
@@ -8,14 +8,25 @@ main(process.argv[2],process.argv[3],process.argv[4])
 
 async function main(inputElementVersion,storeFilenameFrom,storeFilenameTo) {
 	const storeFrom=osm.readStore(storeFilenameFrom)
-	const [etype,eid,ev]=inputElementVersion.split('/')
-	const edata=storeFrom[etype]?.[eid]?.[ev]
-	if (!edata) {
-		console.log(`can't find element ${inputElementVersion}`)
+	const storeTo=osm.readStore(storeFilenameTo)
+	const [etype,eidRange,ev]=inputElementVersion.split('/')
+	const [eid1str,eid2str]=eidRange.split('..')
+	const eid1=Number(eid1str)
+	const eid2=eid2str?Number(eid2str):eid1
+	if (eid2<eid1) {
+		console.log(`invalid element id range ${eid1}..${eid2}`)
 		return
 	}
-	const storeTo=osm.readStore(storeFilenameTo)
-	if (!storeTo[etype][eid]) storeTo[etype][eid]={}
-	storeTo[etype][eid][ev]=edata
+	for (let eid=eid1;eid<=eid2;eid++) {
+		const edata=storeFrom[etype]?.[eid]?.[ev]
+		const etiv=`${etype}/${eid}/${ev}`
+		if (!edata) {
+			console.log(`can't find element ${etiv}`)
+			continue
+		}
+		if (!storeTo[etype][eid]) storeTo[etype][eid]={}
+		storeTo[etype][eid][ev]=edata
+		console.log(`plucked element ${etiv}`)
+	}
 	osm.writeStore(storeFilenameTo,storeTo)
 }
